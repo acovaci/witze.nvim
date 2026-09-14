d76b9dd91b79e8b12c705ef3dac4b6e7db8717f9
acovaci <6562353+acovaci@users.noreply.github.com>
2026-09-14 18:18:45 +0100

    fix: use a function filetype matcher

--- ftdetect/witze.lua
+++ ftdetect/witze.lua
@@ -1,29 +1,25 @@
+local function witze_txt_ft(path, bufnr)
+	local first_line = vim.filetype.getlines(bufnr, 1)
+	if first_line:match("^#%s*witze") then
+		return "witze"
+	end
+	return "text"
+end
+
+local function witze_yml_ft(path, bufnr)
+	local first_line = vim.filetype.getlines(bufnr, 1)
+	if first_line:match("^#%s*witze") then
+		return "witze"
+	end
+	return "yaml"
+end
+
 vim.filetype.add({
 	extension = {
 		mod = "witze",
 		gfx = "witze",
 		gui = "witze",
-		txt = "witze",
-		yml = "witze",
-	},
-	pattern = {
-		[".*%.txt$"] = {
-			priority = math.huge,
-			function(path, bufnr)
-				local first_line = vim.filetype.getlines(bufnr, 1)
-				if first_line:match("^#%s*witze") then
-					return "witze"
-				end
-			end,
-		},
-		[".*%.yml$"] = {
-			priority = math.huge,
-			function(path, bufnr)
-				local first_line = vim.filetype.getlines(bufnr, 1)
-				if first_line:match("^#%s*witze") then
-					return "witze"
-				end
-			end,
-		},
+		txt = witze_txt_ft,
+		yml = witze_yml_ft,
 	},
 })
