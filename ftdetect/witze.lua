22e3de213c90d19de0369e34f3fd32bfb8bc9a31
acovaci <6562353+acovaci@users.noreply.github.com>
2026-09-14 17:51:54 +0100

    fix: lua file detection folder name, also catch yml files with pragma

--- ftdetect/witze.lua
+++ ftdetect/witze.lua
@@ -14,5 +14,14 @@ vim.filetype.add({
 				end
 			end,
 		},
+		[".*%.yml$"] = {
+			priority = math.huge,
+			function(path, bufnr)
+				local first_line = vim.filetype.getlines(bufnr, 1)
+				if vim.filetype.matchregex(first_line, [[^#\s*witze]]) then
+					return "witze"
+				end
+			end,
+		},
 	},
 })
